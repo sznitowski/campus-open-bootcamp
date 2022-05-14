cdbb2b5cf69d412bfa5690018b4436817e668ed2
sznitowski <vsznitowski@gmail.com>
2022-05-14 07:01:11 -0300

    segundo ejercicio

---
+++ Javascript/ejercicios/ejercicio2.js
@@ -0,0 +1,14 @@
+const array = [
+    nombre= "Valentin Sznitowski",
+    edad= 36,
+    desarrollador= true,
+    date= 15/03/1986,
+    libro= {
+        Titulo: "Hobbit",
+        Autor: "John Ronald Reuel Tolkien",
+        fecha: "21/11/1937",
+        url: "https://es.wikipedia.org/wiki/El_hobbit#:~:text=Dispuestos%20a%20publicarla%2C%20los%20editores,1937%20en%20el%20Reino%20Unido."
+    }
+]
+
+console.log(array)
